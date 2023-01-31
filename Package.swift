70cb2289ee15816fb950127a28d4c6fb4c4a2336
Aravind G S <aravind@jiny.io>
2023-01-31 10:19:02 +0530

    Incremented version to 1.7.0

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         .binaryTarget(name: "LeapSDK", path: "LeapSDK.xcframework"),
         .binaryTarget(
             name: "LeapCoreSDK", 
-            url: "https://github.com/Leap-Platform/leap-core-ios/releases/download/1.6.2/LeapCoreSDK.xcframework.zip",
-            checksum: "303c1d73bcd7b1059b701f0108f5b3562243dafc6cd7696b56af338f65bdf7b3"
+            url: "https://github.com/Leap-Platform/leap-core-ios/releases/download/1.7.0/LeapCoreSDK.xcframework.zip",
+            checksum: "39bc57cd436051ab3e566119790f7e4073c5dbef935b960902253beea56a41c7"
         )
     ]
 )
