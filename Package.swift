40d852b8f6255d96f0deae2dfb71474705ceaf11
Aravind G S <aravind@jiny.io>
2023-11-29 17:53:38 +0530

    LeapSDK version 1.11.3 release

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         .binaryTarget(name: "LeapSDK", path: "LeapSDK.xcframework"),
         .binaryTarget(
             name: "LeapCoreSDK", 
-            url: "https://github.com/Leap-Platform/leap-core-ios/releases/download/1.11.2/LeapCoreSDK.xcframework.zip",
-            checksum: "90b09f39af39838d0e24f389ef7d15209acb02e4cd3aaf9dd170391c77db4e7d"
+            url: "https://github.com/Leap-Platform/leap-core-ios/releases/download/1.11.3/LeapCoreSDK.xcframework.zip",
+            checksum: "3031f88101957914687f7686f068bdaf5b9635b9fb941f038c1064c44d250a25"
         )
     ]
 )
