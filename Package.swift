8c77d994e30a866f55e500422d1e4c4f32fe6735
whatfix-dev-jenkins <jenkins@WFX-PC-67.local>
2026-06-20 11:24:12 +0530

    LeapSDK version 2.2.4 release

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
         .binaryTarget(name: "LeapSDK", path: "LeapSDK.xcframework"),
         .binaryTarget(
             name: "LeapCoreSDK", 
-            url: "https://github.com/Leap-Platform/leap-core-ios/releases/download/2.2.3/LeapCoreSDK.xcframework.zip",
-            checksum: "8efc4a025f289c382211bd4dfa09890213eb246a41e68c9dff12dcead49cb903"
+            url: "https://github.com/Leap-Platform/leap-core-ios/releases/download/2.2.4/LeapCoreSDK.xcframework.zip",
+            checksum: "1cfa46fe9edf2c72e934acf4e1190afdfc7d6c77d3d9f8be4335b04af8569226"
         )
     ]
 )
